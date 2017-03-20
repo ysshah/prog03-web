ac990ff404d0c770824d007b26a35cabca1c919b
Yash Shah <yashshah0127@gmail.com>
2017-03-19 22:34:03 -0700

    Change table and column naming

--- recipes/views.py
+++ recipes/views.py
@@ -1,29 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 import boto3
-from boto3.dynamodb.conditions import Key
 
 def index(request):
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('Recipes')
+    table = dynamodb.Table('RecipesDB')
     recipes = table.scan()['Items']
     return render(request, 'index.html', { 'recipes': recipes })
 
 
 def view(request, recipe_name):
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('Recipes')
-    recipes = table.query(KeyConditionExpression=Key(
-        'RecipeName').eq(recipe_name))['Items']
+    table = dynamodb.Table('RecipesDB')
     if request.method == 'POST':
-        table.delete_item(Key={'RecipeName': recipe_name,
-            'Ingredients': recipes[0]['Ingredients']})
+        table.delete_item(Key={'RecipeName': recipe_name})
         return redirect('index')
-    if recipes:
-        recipe = recipes[0]
-        recipe['Ingredients'] = recipe['Ingredients'].split('\n')
-        if 'Directions' in recipe:
-            recipe['Directions'] = recipe['Directions'].split('\n')
+    query = table.get_item(Key={'RecipeName': recipe_name})
+    if 'Item' in query:
+        recipe = query['Item']
+        recipe['Ingredients'] = recipe['IngredientsList'].split('\n')
+        if 'PrepDirections' in recipe:
+            recipe['Directions'] = recipe['PrepDirections'].split('\n')
         context = {
             'recipe': recipe,
             'page_title': recipe['RecipeName'] + ' | '
@@ -34,14 +31,11 @@ def view(request, recipe_name):
 
 def ingredients(request, recipe_name):
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    table = dynamodb.Table('Recipes')
-    recipes = table.query(KeyConditionExpression=Key(
-        'RecipeName').eq(recipe_name))['Items']
-    if recipes:
-        recipe = recipes[0]
-        recipe['Ingredients'] = recipe['Ingredients'].split('\n')
-        if 'Directions' in recipe:
-            recipe['Directions'] = recipe['Directions'].split('\n')
+    table = dynamodb.Table('RecipesDB')
+    query = table.get_item(Key={'RecipeName': recipe_name})
+    if 'Item' in query:
+        recipe = query['Item']
+        recipe['Ingredients'] = recipe['IngredientsList'].split('\n')
         context = {
             'recipe': recipe,
             'page_title': recipe['RecipeName'] + ' Ingredients | '
@@ -53,19 +47,18 @@ def ingredients(request, recipe_name):
 def add(request):
     if request.method == 'POST':
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-        table = dynamodb.Table('Recipes')
+        table = dynamodb.Table('RecipesDB')
         recipe_name = request.POST['RecipeName']
-        recipes = table.query(KeyConditionExpression=Key(
-            'RecipeName').eq(recipe_name))['Items']
-        if not recipes:
+        query = table.get_item(Key={'RecipeName': recipe_name})
+        if 'Item' not in query:
             ingredients = [i for i in request.POST.getlist('Ingredients[]') if i]
             directions = [d for d in request.POST.getlist('Directions[]') if d]
             recipe = {
                 'RecipeName': recipe_name,
-                'Ingredients': '\n'.join(ingredients)
+                'IngredientsList': '\n'.join(ingredients)
             }
             if directions:
-                recipe['Directions'] = '\n'.join(directions)
+                recipe['PrepDirections'] = '\n'.join(directions)
             table.put_item(Item=recipe)
             return redirect('view', recipe_name=recipe_name)
     return render(request, 'add.html', {'page_title': 'Add New Recipe | '})
